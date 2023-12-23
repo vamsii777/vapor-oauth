2560f65cf659f4d0b38faa8d66eb05b7b96696ff
Vamsi Madduluri <vamsi@dewonderstruck.com>
2023-12-23 23:46:15 +0530

    Add generateAccessRefreshTokens method to TokenManager protocol

--- Sources/VaporOAuth/Protocols/TokenManager.swift
+++ Sources/VaporOAuth/Protocols/TokenManager.swift
@@ -10,7 +10,7 @@ public protocol TokenManager: Sendable {
         idTokenExpiryTime: Int,
         nonce: String?
     ) async throws -> (AccessToken, RefreshToken, IDToken)
-
+    
     // Generates only an access token. Should be called after successful authentication.
     func generateAccessToken(
         clientID: String,
@@ -18,16 +18,24 @@ public protocol TokenManager: Sendable {
         scopes: [String]?,
         expiryTime: Int
     ) async throws -> AccessToken
-
+    
+    // Generates both access and refresh tokens. Should be called after successful PKCE validation.
+    func generateAccessRefreshTokens(
+        clientID: String,
+        userID: String?,
+        scopes: [String]?,
+        accessTokenExpiryTime: Int
+    ) async throws -> (AccessToken, RefreshToken)
+    
     // Retrieves a refresh token by its string representation.
     func getRefreshToken(_ refreshToken: String) async throws -> RefreshToken?
-
+    
     // Retrieves an access token by its string representation.
     func getAccessToken(_ accessToken: String) async throws -> AccessToken?
-
+    
     // Updates a refresh token, typically to change its scope.
     func updateRefreshToken(_ refreshToken: RefreshToken, scopes: [String]) async throws
-
+    
     // Generates an ID token. Should be called after successful authentication.
     func generateIDToken(
         clientID: String,
