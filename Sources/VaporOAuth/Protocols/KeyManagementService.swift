0a8b1a350758ee581743cbe6aa5d53676329281e
Vamsi Madduluri <vamsi@dewonderstruck.com>
2023-12-26 03:16:57 +0530

    Add public access and new methods to KeyManagementService

--- Sources/VaporOAuth/Protocols/KeyManagementService.swift
+++ Sources/VaporOAuth/Protocols/KeyManagementService.swift
@@ -1,7 +1,12 @@
-protocol KeyManagementService {
+import Foundation
+import JWTKit
+
+public protocol KeyManagementService {
     func generateKey() throws -> RSAKey
     func storeKey(_ key: RSAKey) throws
     func retrieveKey(identifier: String) throws -> RSAKey
+    func publicKeyIdentifier() throws -> String
+    func convertToJWK(_ key: RSAKey) throws -> JWK 
     // Additional methods for key rotation, deletion, etc.
 
 }
