e140fd4da2a3fd47a7382d253f5371a31823a553
Vamsi Madduluri <vamsi@dewonderstruck.com>
2023-12-26 03:16:48 +0530

    Add JwksHandler to handle JWKS requests

---
+++ Sources/VaporOAuth/RouteHandlers/JwksHandler.swift
@@ -0,0 +1,25 @@
+import Vapor
+import JWTKit
+
+struct JwksHandler {
+    
+    let keyManagementService: KeyManagementService
+    
+    init(keyManagementService: KeyManagementService) {
+        self.keyManagementService = keyManagementService
+    }
+    
+    func handleRequest(_ req: Request) throws -> JWKS {
+        let publicJWKs = try getPublicJWKs()
+        let jwks = JWKS(keys: publicJWKs)
+        return jwks
+    }
+    
+    private func getPublicJWKs() throws -> [JWK] {
+        // Retrieve your public RSA keys using the KeyManagementService
+        let publicKey = try keyManagementService.retrieveKey(identifier: keyManagementService.publicKeyIdentifier())
+        // Convert the RSA public key to JWK format
+        let jwk = try keyManagementService.convertToJWK(publicKey)
+        return [jwk]
+    }
+}
