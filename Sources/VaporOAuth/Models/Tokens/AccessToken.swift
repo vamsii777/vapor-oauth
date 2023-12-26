d5183263146da5f4c996ccf54352dff973f8322b
Vamsi Madduluri <vamsi@dewonderstruck.com>
2023-12-26 17:49:51 +0530

    Add default implementation of verify(using:) for AccessToken

--- Sources/VaporOAuth/Models/Tokens/AccessToken.swift
+++ Sources/VaporOAuth/Models/Tokens/AccessToken.swift
@@ -1,9 +1,17 @@
 import Vapor
+import JWTKit
 
-public protocol AccessToken {
+public protocol AccessToken: JWTPayload {
     var tokenString: String { get }
     var clientID: String { get }
     var userID: String? { get }
     var scopes: [String]? { get }
     var expiryTime: Date { get }
+}
+
+// Providing a default implementation of verify(using:) for AccessToken
+extension AccessToken {
+    public func verify(using signer: JWTSigner) throws {
+        try expiryTime.verifyNotExpired()
+    }
 }
