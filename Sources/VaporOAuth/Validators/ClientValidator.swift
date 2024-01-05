4019c46ffc9d4625d5f5df80036ed2e7188bd600
Vamsi Madduluri <vamsi@dewonderstruck.com>
2024-01-06 03:24:05 +0530

    Add new authorization error types and update client validation logic

--- Sources/VaporOAuth/Validators/ClientValidator.swift
+++ Sources/VaporOAuth/Validators/ClientValidator.swift
@@ -1,73 +1,87 @@
 import Vapor
 
 struct ClientValidator {
-
+    
     let clientRetriever: ClientRetriever
     let scopeValidator: ScopeValidator
     let environment: Environment
-
+    
     func validateClient(clientID: String, responseType: String, redirectURI: String, scopes: [String]?) async throws {
         guard let client = try await clientRetriever.getClient(clientID: clientID) else {
             throw AuthorizationError.invalidClientID
         }
-
+        
         if client.confidentialClient ?? false {
             guard responseType == ResponseType.code else {
                 throw AuthorizationError.confidentialClientTokenGrant
             }
         }
-
+        
         guard client.validateRedirectURI(redirectURI) else {
             throw AuthorizationError.invalidRedirectURI
         }
-
-        if responseType == ResponseType.code {
+        
+        switch responseType {
+        case ResponseType.code:
             guard client.allowedGrantType == .authorization else {
                 throw Abort(.forbidden)
             }
-        } else {
+        case ResponseType.token, ResponseType.idToken, ResponseType.tokenAndIdToken:
+            if client.confidentialClient ?? false {
+                throw AuthorizationError.confidentialClientTokenGrant
+            }
             guard client.allowedGrantType == .implicit else {
                 throw Abort(.forbidden)
             }
+        default:
+            throw AuthorizationError.invalidResponseType
         }
-
+        
         try await scopeValidator.validateScope(clientID: clientID, scopes: scopes)
-
+        
         let redirectURI = URI(stringLiteral: redirectURI)
-
+        
         if environment == .production {
             if redirectURI.scheme != "https" {
                 throw AuthorizationError.httpRedirectURI
             }
         }
     }
-
+    
+    
     func authenticateClient(clientID: String, clientSecret: String?, grantType: OAuthFlowType?,
                             checkConfidentialClient: Bool = false) async throws {
         guard let client = try await clientRetriever.getClient(clientID: clientID) else {
             throw ClientError.unauthorized
         }
-
+        
         guard clientSecret == client.clientSecret else {
             throw ClientError.unauthorized
         }
-
+        
         if let grantType = grantType {
             guard client.allowedGrantType == grantType else {
                 throw Abort(.forbidden)
             }
-
-            if grantType == .password {
-                guard client.firstParty else {
-                    throw ClientError.notFirstParty
-                }
-            }
-            
-            if grantType == .deviceCode {
-                
+            switch grantType {
+            case .authorization:
+                guard client.allowedGrantType == .authorization else { throw Abort(.forbidden) }
+            case .implicit:
+                guard client.allowedGrantType == .implicit else { throw Abort(.forbidden) }
+            case .password:
+                guard client.allowedGrantType == .password else { throw Abort(.forbidden) }
+                if !client.firstParty { throw ClientError.notFirstParty }
+            case .clientCredentials:
+                guard client.allowedGrantType == .clientCredentials else { throw Abort(.forbidden) }
+            case .refresh:
+                guard client.allowedGrantType == .refresh else { throw Abort(.forbidden) }
+            case .tokenIntrospection:
+                guard client.allowedGrantType == .tokenIntrospection else { throw Abort(.forbidden) }
+            case .deviceCode:
+                guard client.allowedGrantType == .deviceCode else { throw Abort(.forbidden) }
             }
         }
-
+        
         if checkConfidentialClient {
             guard client.confidentialClient ?? false else {
                 throw ClientError.notConfidential
--- Tests/VaporOAuthTests/Fakes/FakeKeyManagementService.swift
+++ Tests/VaporOAuthTests/Fakes/FakeKeyManagementService.swift
@@ -4,6 +4,11 @@ import Foundation
 import Crypto
 
 class FakeKeyManagementService: KeyManagementService {
+    
+    func privateKeyIdentifier() throws -> String {
+        return ""
+    }
+    
     func generateKey() throws -> RSAKey {
         // Not required for HMAC based implementation
         fatalError("generateKey() should not be called in FakeKeyManagementService")
