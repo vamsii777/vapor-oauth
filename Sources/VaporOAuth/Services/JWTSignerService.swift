f636ef314890490bdc8ccc00d618ac9f0d93aeac
Vamsi Madduluri <vamsi.madduluri@icloud.com>
2024-01-20 23:47:55 +0530

    Update makeJWTSigner function to support async

--- Sources/VaporOAuth/Services/JWTSignerService.swift
+++ Sources/VaporOAuth/Services/JWTSignerService.swift
@@ -4,5 +4,5 @@ import Foundation
 // Define the protocol
 public protocol JWTSignerService: Sendable {
     var keyManagementService: KeyManagementService { get }
-    func makeJWTSigner() throws -> JWTSigner
+    func makeJWTSigner() async throws -> JWTSigner
 }
